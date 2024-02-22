3bd1e8bb935fe43ebea3e48e80e0083461053b55
PaoloPci <paolopci@yahoo.it>
2024-02-22 16:52:13 +0100

    008 Load Shopping Cart

--- Controllers/CartAPIController.cs
+++ Controllers/CartAPIController.cs
@@ -25,6 +25,37 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         }
 
 
+        [HttpGet("GetCart/{userId}")]
+        public async Task<ResponseDto> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cart = new()
+                {
+                    CartHeader = _mapper.Map<CartHeaderDto>(_db.CartHeaders.First(u => u.UserId == userId)),
+
+                };
+                cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails.Where(x =>
+                    x.CartHeaderId == cart.CartHeader.CartHeaderId));
+
+                foreach (var item in cart.CartDetails)
+                {
+                    cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                }
+                _response.Result = cart;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
@@ -80,7 +111,7 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
 
         }
 
-        [HttpPost("REmoveCart")]
+        [HttpPost("RemoveCart")]
         public async Task<ResponseDto> RemoveCart([FromBody] int cartDetailsId)
         {
             try
